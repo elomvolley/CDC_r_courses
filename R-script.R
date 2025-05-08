31a9faf46c9f8dc337278d89dd6691833f052447
elomvolley <elomvolley@gmail.com>
2025-05-08 15:20:49 -0400

    test.2

--- R-script.R
+++ R-script.R
@@ -1,1 +1,10 @@
-data <- c("hi world")
+packages.install("dplyr")
+library(dplyr)
+
+c= c(1,2,3)
+
+data <- paste(c, y)
+print(data)
+save(y, file="y.Rdata")
+setwd("C:/Users/tqd7/OneDrive - CDC/GitHub/CDC_r_courses")
+
